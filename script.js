9a7932ad8188a87ac21d56506c378fee149a9d57
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-24 22:58:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let promp=confirm("Do you want to proceed?")
+
+if (promp) {
+	alert("You clicked OK. Proceeding...")
+}else{
+	alert("You clicked Cancel. Exiting...")
+}
